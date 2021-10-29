6a07a59fe68e664785a5eee9aaa56066db29fc09
Christina MacKenzie <christina@themackenzies.net>
2021-10-28 22:27:07 -0400

    tooltip fix

--- mapChart.js
+++ mapChart.js
@@ -116,8 +116,8 @@ export default function MapChart(container, data, world) {
                 console.log(pos)
     
                 d3.select("#map-tooltip")
-                    .style('left', pos[0] - 100 + "px")
-                    .style('top', pos[1] - 100 + "px")
+                    .style('left', pos[0] + "px")
+                    .style('top', pos[1] + "px")
                     .html(() => {
                         if (d.properties.Value === null) {
                             return `Country: ${d.properties.name} <br>
