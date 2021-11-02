5ded4d949389c7fcc06adae6cc9d1df1b4355022
Christina MacKenzie <christina@themackenzies.net>
2021-11-02 18:03:19 -0400

    bar chart fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import MapChart from './mapChart.js';
-import barChart from './species_chart.js';
+import BarChart from './species_chart.js';
 
 d3.csv("Threatened_species.csv", d3.autoType).then(data => {
     d3.json("world-110m.json", d3.autoType).then(world => {
@@ -7,19 +7,21 @@ d3.csv("Threatened_species.csv", d3.autoType).then(data => {
         // Generate initial map and update it to data from 2004
         const mapChart = MapChart(".map", data, world)
         mapChart.update(data, 2004)
+        const barChart = BarChart(".bar-chart")
+
+        let country = null
+        let selection = document.getElementById('year')
 
         // Listen for change in year dropdown, update charts if changed
-        let selection = document.getElementById('year')
         selection.onchange = () => {
             mapChart.update(data, selection.value)
-            }
-        
+            barChart.update(data, selection.value, country)
+        }
         
         // Update other charts when country is clicked on map
-        mapChart.on("click", (country) => {
-            d3.select("#svg-bar").selectAll("svg").exit().remove();
-            let BarChart = barChart(".bar-chart", selection.value, country, data)
-            
+        mapChart.on("click", (c) => {
+            country = c
+            barChart.update(data, selection.value, country)
         })
     })
 
--- species_chart.js
+++ species_chart.js
@@ -1,94 +1,106 @@
-export default function barChart(container, year, country, data) {
-    console.log(year)
-    console.log(country)
-   
-   
-    const filteredData = data.filter(d=> d.Country == country &&d.Year == year &&d.Series != "Threatened Species: Total (number)");
+export default function BarChart(container) {
 
+  const margin = {top: 20, right: 20, bottom: 70, left: 75};
+  const width = 500 - margin.left - margin.right;
+  const height = 300 - margin.top - margin.bottom;
 
-    const margin = {top: 20, right: 20, bottom: 70, left: 75};
-    const width = 500 - margin.left - margin.right;
-    const height = 300 - margin.top - margin.bottom;
+  const xScale = d3.scaleBand()
+    .range([0, width]);
+  const yScale = d3.scaleLinear()
+    .range([height, 0]);    
 
-    const xScale = d3.scaleBand().domain(filteredData.map(function(d) {return d.Series.substring(20,((d.Series.length) - 9));})).range([0, width]);
-    const yScale = d3.scaleLinear().domain([0, parseFloat(d3.max(filteredData, d=>d.Value))]).range([height, 0]);
-  
-    d3.select('#svg-bar').remove();
-    
+  const xAxis = d3.axisBottom().scale(xScale)
+  const yAxis = d3.axisLeft().scale(yScale); 
 
-    const xAxis = d3.axisBottom().scale(xScale)
-    const yAxis = d3.axisLeft().scale(yScale); 
+  const svg = d3
+    .select(container)
+    .attr("class", "svg-bar")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
 
+  const x_axis = svg.append("g")
+    .attr("class", "x-axis")
+    .call(xAxis)
+    .attr("transform", `translate(${margin.left - 10 }, ${height+margin.top+ 15})`);
 
-      const svg = d3
-        .select(container)
-        .attr("class", "svg-bar")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        
+  const y_axis = svg.append("g")
+    .attr("class", "y-axis")
+    .call(yAxis)
+    .attr("transform", `translate(${margin.left - 10} , ${margin.top +15 })`);
 
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -140)
+    .attr("y", 15)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Number of Threatened Species");  
 
-svg.append("g")
-.attr("class", "x-axis")
-.call(xAxis)
-.attr("transform", `translate(${margin.left - 10 }, ${height+margin.top+ 15})`);
+  svg.append("text")             
+    .attr("transform",
+          "translate(" + (width/2 + 60) + " ," + (height + margin.top + 50) + ")")
+    .style("text-anchor", "middle")
+    .text("Species Type");
 
-svg.append("g")
-.attr("class", "y-axis")
-.call(yAxis)
-.attr("transform", `translate(${margin.left - 10} , ${margin.top +15 })`);
+  const heading = svg.append("text")
+    .attr("class", "axis-title")
+    .attr("transform",
+          "translate(" + (width/2 + 60) + " ," + (height + margin.top - 205) + ")")
+    .attr("text-anchor", "middle")
+    .attr("font-weight", "bold")
 
-svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -140)
-        .attr("y", 15)
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Number of Threatened Species");  
+  function update(data, year, country) {
 
-      svg.append("text")             
-        .attr("transform",
-              "translate(" + (width/2 + 60) + " ," + (height + margin.top + 50) + ")")
-        .style("text-anchor", "middle")
-        .text("Species Type");
+    if (country != null) 
+      {const filteredData = data.filter(d => d.Country == country && d.Year == year && d.Series != "Threatened Species: Total (number)");
 
-      svg.append("text")
-        .attr("class", "axis-title")
-        .attr("transform",
-              "translate(" + (width/2 + 60) + " ," + (height + margin.top - 205) + ")")
-        .attr("text-anchor", "middle")
-        .attr("font-weight", "bold")
-        .text("Threatened Species in " + country + " in " + year)
-        
+      xScale.domain(filteredData.map(function(d) {return d.Series.substring(20,((d.Series.length) - 9));}))
+      yScale.domain([0, parseFloat(d3.max(filteredData, d=>d.Value))])
 
-  const bars = svg.selectAll(".bar") .data(filteredData)
-  
-  bars
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", (function(d) { return xScale(d.Series.substring(20,((d.Series.length) - 9)))}))
-    .attr("y", (function(d) { return yScale(d.Value)}))
-    .attr("width", 100)
-    .attr("height", function(d) { return height - yScale(d.Value); })
-    .attr("transform", `translate(${margin.left + 5} , ${margin.top +15 })`)
-    .attr("fill", "lightblue")
-    .on("mouseover", function(event, d) {
-      const pos = d3.pointer(event, window);
-      d3.select("#bar-tooltip")
-          .style("left", pos[0] + "px")
-          .style("top", pos[1] + "px")
-        .select("#value")
-        .html(d.Value + " Threatened " + d.Series.substring(20,((d.Series.length) - 9)))      
-      d3.select("#bar-tooltip").classed("hidden", false);
-    })
-    .on("mouseout", function(d) {
-      d3.select("#bar-tooltip").classed("hidden", true);
-    });
+      x_axis.transition()
+        .call(xAxis)
 
-    
-    bars.exit().remove();
+      y_axis.transition()
+        .call(yAxis)
 
+      heading.text("Threatened Species in " + country + " in " + year)
+          
+      const bars = svg.selectAll("rect") 
+        .data(filteredData)
+
+      bars
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .merge(bars)
+        .transition()
+        .attr("x", (function(d) { return xScale(d.Series.substring(20,((d.Series.length) - 9)))}))
+        .attr("y", (function(d) { return yScale(d.Value)}))
+        .attr("width", 100)
+        .attr("height", function(d) { return height - yScale(d.Value); })
+        .attr("transform", `translate(${margin.left + 5} , ${margin.top +15 })`)
+        .attr("fill", "lightblue")
+      
+      bars.exit().remove();
+
+      bars.on("mouseover", function(event, d) {
+        const pos = d3.pointer(event, window);
+        d3.select("#bar-tooltip")
+            .style("left", pos[0] + "px")
+            .style("top", pos[1] + "px")
+          .select("#value")
+          .html(d.Value + " Threatened " + d.Series.substring(20,((d.Series.length) - 9)))      
+        d3.select("#bar-tooltip").classed("hidden", false);
+      })
+      .on("mouseout", function(d) {
+        d3.select("#bar-tooltip").classed("hidden", true);
+      });
+    }
+  }
+
+  return {
+    update
+  }
 }
